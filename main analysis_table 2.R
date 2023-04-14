2a3b94ac401469612adebfc9a8a9078b17693284
Hung Pham <115704343+hpham22@users.noreply.github.com>
2023-04-15 01:31:12 +1000

    Update main analysis_table 2.R

--- main analysis_table 2.R
+++ main analysis_table 2.R
@@ -5,13 +5,13 @@ library(stargazer)
 # Regression model for all cases
 load("data/asean_del.rda")
 
-model_1 <- glm.nb(ndelms ~ kofecgidjsdv + polityivsdv + avzcomplexv + naseanms + type + media + pol, 
+model_1 <- glm.nb(ndelms ~ kofecgidjsdv + polityivsdv + avzcomplex + naseanms + type + media + pol, 
                   data = asean_del)
   
 # Regression model for economic cases
 load("data/asean_del_econ.rda")
   
-model_2 <- glm.nb(ndelms ~ kofecgidjsdv + polityivsdv + avzcomplexv + naseanms + type + media, 
+model_2 <- glm.nb(ndelms ~ kofecgidjsdv + polityivsdv + avzcomplex + naseanms + type + media, 
                   data = asean_del_econ)
 
 stargazer(model_1, model_2, type = "html", single.row = TRUE, out = "Table 2.doc")
