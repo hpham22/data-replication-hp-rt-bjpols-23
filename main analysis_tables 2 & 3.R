1a571edf71aec239f08f816411be6f140840be85
Hung Pham <115704343+hpham22@users.noreply.github.com>
2024-03-15 11:08:03 +1100

    Update main analysis_tables 2 & 3.R

--- main analysis_tables 2 & 3.R
+++ main analysis_tables 2 & 3.R
@@ -1,13 +1,13 @@
 library(MASS)
 library(stargazer)
 
-# Regression model for all cases - Table 2
+# Regression model for full case analysis - Table 2
 load("data/asean_del.rda") # main dataset
 
 model_1_tbl2 <- glm.nb(ndelms ~ kofecgidjsdv + polityivsdv + avzcomplex + naseanms + type + media + pol, 
                   data = asean_del)
 
-stargazer(model_1, type = "html", single.row = TRUE, digits = 2, out = "Table 2.doc")
+stargazer(model_1_tbl2, type = "html", single.row = TRUE, digits = 2, out = "Table 2.doc")
   
 # Regression models for sub-sample analysis (economic vs. non-economic cases) - Table 3
 load("data/asean_del_econ.rda") # sub-dataset for economic cases
